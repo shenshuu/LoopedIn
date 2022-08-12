b0e29c4534a871ac8dc7f723a88e8b4daba941c0
Michael Shen <michaelshen85@gmail.com>
2022-08-12 17:28:07 -0400

    Add session and user routes

--- LoopedIn/app/controllers/api/users_controller.rb
+++ LoopedIn/app/controllers/api/users_controller.rb
@@ -1,3 +1,16 @@
 class Api::UsersController < ApplicationRecord
-    
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            render :show 
+        else
+            
+        end
+    end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:email, :password)
+    end
 end
