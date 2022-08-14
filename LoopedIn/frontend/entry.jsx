e6d98a72428f3d75b05da97a10d609861b7345ce
Michael Shen <michaelshen85@gmail.com>
2022-08-14 16:34:29 -0400

    Add store

--- LoopedIn/frontend/entry.jsx
+++ LoopedIn/frontend/entry.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import {login, logout, signup} from './actions/session_actions';
+
+window.login = login;
+window.logout = logout;
+window.signup = signup;
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
+    window.store = store;
     const root = document.getElementById('root');
     ReactDOM.render(<h1>H</h1>, root);
 });
