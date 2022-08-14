413db37c69265fe8d716be9f78079ac0de998718
Michael Shen <michaelshen85@gmail.com>
2022-08-14 16:43:39 -0400

    Add Root and App components

--- LoopedIn/frontend/entry.jsx
+++ LoopedIn/frontend/entry.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import {login, logout, signup} from './actions/session_actions';
+import Root from './components/root';
 
 window.login = login;
 window.logout = logout;
@@ -11,5 +12,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
     window.store = store;
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>H</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
