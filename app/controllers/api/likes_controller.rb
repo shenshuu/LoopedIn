5edb7b978e2820b9b97b4255a17df8e0b4877a55
Michael Shen <michaelshen85@gmail.com>
2022-08-19 14:53:18 -0400

    Finish setting up likes backend

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,3 +1,30 @@
 class Api::LikesController < ApplicationController 
-    
+    def index 
+        @likes = Like.all 
+        render :index 
+    end
+
+    def create 
+        @like = Like.new(like_params)
+        @like.user_id = current_user.id if current_user 
+        if @like.save 
+            render :show 
+        else
+            render json: @like.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy 
+        @like = Like.find_by(id: params[:id])
+        if @like.destroy 
+            render json: ['successful unlike']
+        else
+            render json: ['unsuccessful unlike']
+        end
+    end
+
+    private 
+    def like_params 
+        params.require(:like).permit(:user_id, :post_id, :comment_id)
+    end
 end
